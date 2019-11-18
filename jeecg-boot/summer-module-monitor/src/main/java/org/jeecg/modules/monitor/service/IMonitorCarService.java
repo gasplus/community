796bce19cbfe2e96ab2cf99d6bc7d67cdb6018cc
gaspplus <275132103@qq.com>
2019-11-18 14:48:49 +0800

    初始化

---
+++ jeecg-boot/summer-module-monitor/src/main/java/org/jeecg/modules/monitor/service/IMonitorCarService.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.monitor.service;
+
+import org.jeecg.modules.monitor.entity.MonitorCar;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: monitor_car
+ * @Author: jeecg-boot
+ * @Date:   2019-11-18
+ * @Version: V1.0
+ */
+public interface IMonitorCarService extends IService<MonitorCar> {
+
+}
