dd379aace4d04890d72efe068739c77a4b301b8f
gaspplus <275132103@qq.com>
2019-11-18 14:48:30 +0800

    初始化

---
+++ jeecg-boot/summer-module-monitor/src/main/java/org/jeecg/modules/monitor/mapper/MonitorCarMapper.java
@@ -0,0 +1,17 @@
+package org.jeecg.modules.monitor.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.jeecg.modules.monitor.entity.MonitorCar;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: monitor_car
+ * @Author: jeecg-boot
+ * @Date:   2019-11-18
+ * @Version: V1.0
+ */
+public interface MonitorCarMapper extends BaseMapper<MonitorCar> {
+
+}
---
+++ jeecg-boot/summer-module-monitor/src/main/java/org/jeecg/modules/monitor/service/impl/MonitorCarServiceImpl.java
@@ -0,0 +1,19 @@
+package org.jeecg.modules.monitor.service.impl;
+
+import org.jeecg.modules.monitor.entity.MonitorCar;
+import org.jeecg.modules.monitor.mapper.MonitorCarMapper;
+import org.jeecg.modules.monitor.service.IMonitorCarService;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * @Description: monitor_car
+ * @Author: jeecg-boot
+ * @Date:   2019-11-18
+ * @Version: V1.0
+ */
+@Service
+public class MonitorCarServiceImpl extends ServiceImpl<MonitorCarMapper, MonitorCar> implements IMonitorCarService {
+
+}
