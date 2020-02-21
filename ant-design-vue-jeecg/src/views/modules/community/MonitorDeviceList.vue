91e089b4c78d7d229db822cba4167e4cdfb8c4cf
gasplus <275132103@qq.com>
2020-02-21 15:11:01 +0800

    增加模糊查询

--- ant-design-vue-jeecg/src/views/modules/community/MonitorDeviceList.vue
+++ ant-design-vue-jeecg/src/views/modules/community/MonitorDeviceList.vue
@@ -24,7 +24,7 @@
 
           <a-col :span="6">
             <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
-              <a-button type="primary" @click="searchByquery" icon="search">查询</a-button>
+              <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
               <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
             </span>
           </a-col>
